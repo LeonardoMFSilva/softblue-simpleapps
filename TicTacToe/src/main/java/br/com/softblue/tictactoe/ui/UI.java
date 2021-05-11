b41d549c69a46bde9c514b4eaf30ee19b21d0b43
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-05-11 11:44:23 -0300

    Adding UI class methods

--- TicTacToe/src/main/java/br/com/softblue/tictactoe/ui/UI.java
+++ TicTacToe/src/main/java/br/com/softblue/tictactoe/ui/UI.java
@@ -1,4 +1,30 @@
 package br.com.softblue.tictactoe.ui;
 
+import java.util.Scanner;
+
 public class UI {
+    public void printText(String text){
+        System.out.println(text);
+    }
+
+    public void printTextWithNoNewLine(String text) {
+        System.out.print(text);
+    }
+
+    public void printNewLine(){
+        System.out.println();
+    } // usado para quebra de linha, usando apenas o sout/sysout
+
+    public void printGameTitle(){
+        printText("============");
+        printText("| Jogo da Velha |");
+        printText("============");
+        printNewLine();
+    }
+
+    String readInput (String text){
+        Scanner ler = new Scanner(System.in);
+        printTextWithNoNewLine(text + " ");
+        return ler.next();
+    }
 }
