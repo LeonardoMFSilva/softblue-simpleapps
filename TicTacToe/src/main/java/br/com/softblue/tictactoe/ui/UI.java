dae3b3b05dfbc7d6f1202db043c267fa49904de4
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-05-11 11:47:51 -0300

    Correcting UI class methods

--- TicTacToe/src/main/java/br/com/softblue/tictactoe/ui/UI.java
+++ TicTacToe/src/main/java/br/com/softblue/tictactoe/ui/UI.java
@@ -3,26 +3,26 @@ package br.com.softblue.tictactoe.ui;
 import java.util.Scanner;
 
 public class UI {
-    public void printText(String text){
+    public static void printText(String text){
         System.out.println(text);
     }
 
-    public void printTextWithNoNewLine(String text) {
+    public static void printTextWithNoNewLine(String text) {
         System.out.print(text);
     }
 
-    public void printNewLine(){
+    public static void printNewLine(){
         System.out.println();
     } // usado para quebra de linha, usando apenas o sout/sysout
 
-    public void printGameTitle(){
+    public static void printGameTitle(){
         printText("============");
         printText("| Jogo da Velha |");
         printText("============");
         printNewLine();
     }
 
-    String readInput (String text){
+    public static String readInput (String text){
         Scanner ler = new Scanner(System.in);
         printTextWithNoNewLine(text + " ");
         return ler.next();
