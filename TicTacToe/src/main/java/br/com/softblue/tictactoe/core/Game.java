81e272de86e11c3a5343021841c5ab4e04c326c6
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-05-17 11:14:39 -0300

    Checking the next player to play

--- TicTacToe/src/main/java/br/com/softblue/tictactoe/core/Game.java
+++ TicTacToe/src/main/java/br/com/softblue/tictactoe/core/Game.java
@@ -7,7 +7,7 @@ public class Game {
 
     private Board board = new Board();
     private Player[] players = new Player[Constants.SYMBOL_PLAYERS.length];
-
+    private int currentPlayerIndex = 0;
 
     public void play(){
         UI.printGameTitle();
@@ -25,5 +25,29 @@ public class Game {
         UI.printText("O jogador " + name + " vai usar o símbolo " + symbol);
 
         return player;
+    }
+
+    private Player nextPlayer (){
+        // Duas forma de fazer, a primeira com o if
+
+        /* currentPlayerIndex++; // necessário para a saber qual jogador é o atual
+          if (currentPlayerIndex >= players.length){
+              currentPlayerIndex = 0;
+          }
+
+          return players[currentPlayerIndex];*/
+
+        // A segunda com o índice circular:
+        // Dividindo o valor atual pelo tamanho total do Array ou indice
+        // E Pegando o resto da divisão
+        // quando ele se dividir por si mesmo (ou pelo tamanho total dele)
+        // ele irá retonar ao indice 0.
+        // EX: Indice de 3...
+        // 0/3 resto = 0 (primeiro indice)
+        // 1/3 resto = 1 (segundo indice)
+        // 2/3 resto = 2 (terceiro indice)
+        // 3/3 resto = 0 (primeiro indice)
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+        return players[currentPlayerIndex];
     }
 }
