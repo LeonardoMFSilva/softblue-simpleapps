4db3171718c8173d3bc944ce2b1a82e0ab53f641
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-05-17 10:09:02 -0300

    Creating players

--- TicTacToe/src/main/java/br/com/softblue/tictactoe/core/Game.java
+++ TicTacToe/src/main/java/br/com/softblue/tictactoe/core/Game.java
@@ -11,8 +11,19 @@ public class Game {
 
     public void play(){
         UI.printGameTitle();
-        board.clear();
-        board.print();
-        //UI.readInput("Nome do jogador:");
+
+        for (int i = 0; i < players.length; i++) {
+           players[i] = createPlayer(i);
+
+        }
+    }
+
+    private Player createPlayer(int index){
+        String name = UI.readInput("Jogador" + (index + 1 ) + "=>");
+        char symbol = Constants.SYMBOL_PLAYERS[index];
+        Player player = new Player(name, symbol, board);
+        UI.printText("O jogador " + name + " vai usar o símbolo " + symbol);
+
+        return player;
     }
 }
