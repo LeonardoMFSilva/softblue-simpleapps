514d4437b7536f34f4dd8047254a6c4d8f2ffd6e
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-06-02 09:07:17 -0300

    Creating first methods of Word class

--- HangMan/src/br/com/softblue/jogoforca/core/Word.java
+++ HangMan/src/br/com/softblue/jogoforca/core/Word.java
@@ -1,4 +1,28 @@
 package br.com.softblue.jogoforca.core;
 
 public class Word {
+
+    private static final char SECRET_CHAR = '_';
+
+    private String originalWord;
+
+    public Word(String originalWord) {
+        this.originalWord = originalWord;
+    }
+
+    public int size(){
+        return originalWord.length();
+    }
+
+    public boolean hasChar(char c){
+        if (originalWord.indexOf(c)> -1){
+            return true;
+        }
+
+        return false;
+    }
+
+    public String getOriginalWord() {
+        return originalWord;
+    }
 }
