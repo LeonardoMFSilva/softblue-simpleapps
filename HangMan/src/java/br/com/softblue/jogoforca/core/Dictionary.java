a644a226ffd20fe14065bbd0e1d3defbf64efb74
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-06-04 21:48:52 -0300

    Creating first methods for Dictionary class

--- HangMan/src/java/br/com/softblue/jogoforca/core/Dictionary.java
+++ HangMan/src/java/br/com/softblue/jogoforca/core/Dictionary.java
@@ -1,5 +1,22 @@
 package br.com.softblue.jogoforca.core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Dictionary {
+
+    private static final String FILE_NAME = "dicionario.txt";
+    private List<String> words = new ArrayList<>();
+
+    private void load(){
+       try ( Scanner scanner =
+                     new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))) {
+           while (scanner.hasNextLine()) {
+               String word = scanner.nextLine().trim();
+           }
+       }
+    }
+
 
 }
