f36ebfdd69df0309d690862f9476664213f1de0c
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-06-05 18:25:40 -0300

    Adding UI class and Invalid Character Exception

---
+++ HangMan/src/java/br/com/softblue/jogoforca/core/InvalidCharacterException.java
@@ -0,0 +1,7 @@
+package br.com.softblue.jogoforca.core;
+
+public class InvalidCharacterException extends Exception{
+    public InvalidCharacterException(String message) {
+        super(message);
+    }
+}
