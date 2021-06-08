c38fc3c01dd13b9ee445b3c5f0fd4de8ec079d52
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-06-08 08:11:56 -0300

    Dictionary selection from config file

--- HangMan/src/java/br/com/softblue/jogoforca/core/Dictionary.java
+++ HangMan/src/java/br/com/softblue/jogoforca/core/Dictionary.java
@@ -3,6 +3,8 @@ package br.com.softblue.jogoforca.core;
 import br.com.softblue.jogoforca.game.GameException;
 import br.com.softblue.jogoforca.utils.RandomUtils;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,7 +16,14 @@ public abstract class Dictionary {
 
     public static Dictionary getInstance(){
         if (instance == null){
-            instance = new StaticDictionary();
+           try {
+               String dictionaryClassName = Config.get("dictionaryClassName");
+               Class<?> clazz = Class.forName(dictionaryClassName);
+               Constructor<?> constructor = clazz.getConstructor();
+               instance = (Dictionary) constructor.newInstance();
+           } catch (Exception e) {
+               throw new RuntimeException(e);
+           }
         }
 
         return instance;
