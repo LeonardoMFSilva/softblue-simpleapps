6227c205b8129d1a1d6eed848a6889fb4f759287
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-06-17 07:56:37 -0300

    Method References

--- lambda&streamapi/streamapi/src/main/java/br/com/softblue/streamapi/Main.java
+++ lambda&streamapi/streamapi/src/main/java/br/com/softblue/streamapi/Main.java
@@ -24,7 +24,12 @@ public class Main {
               .map(w -> w.toLowerCase())
               .forEach(w -> System.out.println(w)); // com o forEach tb é possível fazer sem retonar uma lista, como ele irá percorrer e mostrar todos os elementos especificados
 
-
-
+        //Reference Methods - referencia o método chamado para simplificar. Usa-se Method References em duas situações:
+        //1 - Quando o parametro recebido estiver sendo passado (não quando está sendo definido)_como parametro
+        //2 - Quando usar o paramentro recebido como parametro para chamar um metodo (que irá utilizar o parametro)
+        names.stream()
+                .filter(w -> w.startsWith("L")) //definição do parametro
+                .map(String::toLowerCase) // paramentro sendo passado com Method Reference
+                .forEach(System.out::println); // parametro usado e recebido sendo chamado para o metodo
     }
 }
